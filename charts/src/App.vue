af3b5eca25d525a14941efb2c2ed36cbed8c18e0
Jens Van den Eeckhout <jens.vandeneeckhout@student.odisee.be>
2019-12-21 15:36:01 +0100

    vue charts - MS: the beginning of something great

---
+++ charts/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+
+
+  <line-chart :data="chartData"></line-chart>
+
+<pie-chart :data="[['Nokia',150],['Samsung',120]]"></pie-chart>
+
+
+<button @click="updateChart"> Update Data</button>
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'app',
+  data(){
+    return{
+      chartData: {
+        '2017-05-13':2,
+        '2017-05-14':5,
+        '2017-05-15':4
+      }
+    }
+  },
+  methods:{
+    updateChart(){
+      this.chartData = {
+        '2017-05-13':8,
+        '2017-05-14':15,
+        '2017-05-15':11       
+      }
+    }
+  }
+
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
